6265f4e03a963b896ee874cdd1f0b3554a1c61c4
baldo-vela <baldo.vela+github@gmail.com>
2021-07-14 01:03:32 -0500

    Updates Routes

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,4 +1,24 @@
 class Api::V1::AuthController < ApplicationController
     def spotify_request
+        url = "https://accounts.spotify.com/authorize"
+        query_params = {
+            client_id: Rails.application.credentials[Rails.env.to_sym][ENV["spotify_id"]][ENV["client_id"]],
+            response_type: 'code',
+            redirect_uri: 'http://localhost:3000/api/v1/user',
+            scope: "user-library-read 
+                    playlist-read-collaborative
+                    playlist-modify-private
+                    user-modify-playback-state
+                    user-read-private
+                    user-top-read
+                    playlist-modify-public
+                    user-read-recently-played
+                    user-read-playback-state
+                    user-read-currently-playing",
+            show_dialog: true
+        }
+    redirect_to "#{url}?#{query_params.to_query}"
+  end
+end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,38 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  #Model Related Routes
+  namespace :api do
+    namespace :v1 do
+      resources :tracks
+    end
+  end
+
+  namespace :api do
+    namespace :v1 do
+      resources :users
+    end
+  end
+  namespace :api do
+    namespace :v1 do
+      resources :playlists
+    end
+  end
+  namespace :api do
+    namespace :v1 do
+      resources :game_logs
+    end 
+  end
+  
+  #Auth Related Routes
+  namespace :api do
+    namespace :v1 do
+      get '/login', to: "auth#spotify_request"
+      get '/auth', to: "auth#show"
+      get '/user', to: "users#create"
+      patch '/user', to: "users#update"
+    end
+  end
+
 end
+
