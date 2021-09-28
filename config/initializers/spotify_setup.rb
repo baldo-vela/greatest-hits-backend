1650981faaab88ae46866fec9a6318d534b0a7c1
baldo-vela <74266847+baldo-vela@users.noreply.github.com>
2021-09-28 12:18:05 -0500

    Reimplements RSpotify config

--- config/initializers/spotify_setup.rb
+++ config/initializers/spotify_setup.rb
@@ -1,2 +1,2 @@
 #Configures the RSpotify gem with encrypted credentials
-# RSpotify::authenticate(Rails.application.credentials.spotify[:spotify_id], Rails.application.credentials.spotify["spotify_secret"])
+RSpotify::authenticate(Rails.application.credentials.spotify[:spotify_id], Rails.application.credentials.spotify[:spotify_secret])
