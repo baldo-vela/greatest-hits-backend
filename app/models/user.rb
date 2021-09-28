f9902ee7023d7db6f9d90b04bfa2db8f00b62cbd
baldo-vela <74266847+baldo-vela@users.noreply.github.com>
2021-09-28 12:18:05 -0500

    Reformats auth token refreshing

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,14 +9,22 @@ class User < ApplicationRecord
     end
 
     def refresh_access_token
+        # See #4 'Requesting a Refreshed access token' https://developer.spotify.com/documentation/general/guides/authorization-guide/ 
         if access_token_expired?
+            spotify_token_url = "https://accounts.spotify.com/api/token"
+            key = "#{Rails.application.credentials.spotify[:spotify_id]}:#{Rails.application.credentials.spotify[:spotify_secret]}"
+            header = { 
+                Authorization: "Basic #{Base64.strict_encode64(key)}"
+                #Note, it must be `strict_encode64` not `encode64`, because the later includes a line return that would have to be pruned.
+            }
             body = {
                 grant_type: 'refresh_token',
-                spotify_refresh_token: self.spotify_refresh_token,
-                client_id: Rails.application.credentials.spotify[:client_id],
-                client_secret: Rails.application.credentials.spotify[:client_secret]
+                spotify_refresh_token: self.spotify_refresh_token
+                #client_id: Rails.application.credentials.spotify[:spotify_id]
+                #client_secret: Rails.application.credentials.spotify[:spotify_secret]
             }
-            auth_response = RestClient.post("https://accounts.spotify.com/api/token", body)
+            debugger
+            auth_response = RestClient.post(spotify_token_url, body, header)
             auth_params = JSON.parse(auth_response)
             self.update(spotify_access_token: auth_params["access_token"])
 
