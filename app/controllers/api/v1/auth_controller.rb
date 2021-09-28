224fa296f9565b98445a7b4a7b040c71b4739ad5
baldo-vela <74266847+baldo-vela@users.noreply.github.com>
2021-09-28 12:18:05 -0500

    Adds annotation

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,10 +1,14 @@
 class Api::V1::AuthController < ApplicationController
+    # When the client sends a request to 'api/v1/login' routes to this method
     def spotify_request
+        # Spotify API Endpoint
         url = "https://accounts.spotify.com/authorize"
         query_params = {
             client_id: Rails.application.credentials.spotify[:spotify_id],
+            # Spotify_id is encoded in the Rails credentials file
             response_type: 'code',
             redirect_uri: 'http://localhost:3001/api/v1/user',
+            # Tells the Spotify API where to send the client after the user logs in, See 'api/v1/user_controller.rb'
             scope: "user-library-read 
                     playlist-read-collaborative
                     playlist-modify-private
@@ -17,6 +21,7 @@ class Api::V1::AuthController < ApplicationController
                     user-read-currently-playing",
             show_dialog: true
         }
+    #Sends the client to the Spotify API with the query params
     redirect_to "#{url}?#{query_params.to_query}"
   end
 end
