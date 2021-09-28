0f0ccbe82c66947d0d8eeeaf70f3f8440f20c293
baldo-vela <74266847+baldo-vela@users.noreply.github.com>
2021-09-28 12:18:05 -0500

    Temporarily Disables spotifyconfig in new RTE

--- config/initializers/spotify_setup.rb
+++ config/initializers/spotify_setup.rb
@@ -1,2 +1,2 @@
 #Configures the RSpotify gem with the Figaro shimmed and hidden keys from `./application.yml`
-RSpotify::authenticate(ENV["spotify_id"], ENV["spotify_secret"])
+# RSpotify::authenticate(ENV["spotify_id"], ENV["spotify_secret"])
