6c2fdd5dbc2477f9eb9e62df7ab89debb3d86581
baldo-vela <74266847+baldo-vela@users.noreply.github.com>
2021-11-07 23:27:43 -0600

    Updates note associations and validations

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,7 +1,9 @@
 class Note < ApplicationRecord
-    belongs_to :user,
+    # belongs_to :user,
     belongs_to :playlist
-
-    validates :body, presence :true #All notes should have content
+    # Note: we don't need user association anymore since we aren't pushing a user from the client, just their spotify name
+    validates :content, presence :true
+    validates :username, presence :true
+    validates :playlist_id, presence :true #All notes should have content
 
 end
