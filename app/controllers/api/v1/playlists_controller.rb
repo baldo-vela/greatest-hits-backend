51ae8b8b4eab71bddd4201bfc83eae3bda992b35
baldo-vela <74266847+baldo-vela@users.noreply.github.com>
2021-11-08 15:19:18 -0600

    Adds in basics for playlists

--- app/controllers/api/v1/playlists_controller.rb
+++ app/controllers/api/v1/playlists_controller.rb
@@ -1,6 +1,30 @@
-require 'rest_client'
-
-class Api::V1::PlaylistController < ApplicationController
+class Api::V1::PlaylistsController < ApplicationController
     def index
         @playlists = Playlist.all
+
+        render json: @playlists, include: {
+            notes: {}
+        }
     end
+
+    def show
+        @playlist = Playlist.find_by(spotify_id: params[:spotify_id])
+        list_of_notes = @playlist.Notes
+        if @playlist
+            render json: @playlist, include: :notes
+        else
+            render json: {error: 'Playlist Not Found'}
+        end
+
+    end
+
+    def create
+        @playlist = Playlist.build(id: playlist_params[:spotify_id])
+    end
+
+
+    private
+    def playlist_params
+        params.require(:)
+    end
+end
