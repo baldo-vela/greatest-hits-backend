807b6aa2b24a79378b7c871149469e741a14da11
baldo-vela <74266847+baldo-vela@users.noreply.github.com>
2021-11-08 09:21:32 -0600

    Temporarily disables validations

--- app/models/note.rb
+++ app/models/note.rb
@@ -2,8 +2,8 @@ class Note < ApplicationRecord
     # belongs_to :user,
     belongs_to :playlist
     # Note: we don't need user association anymore since we aren't pushing a user from the client, just their spotify name
-    validates :content, presence :true
-    validates :username, presence :true
-    validates :playlist_id, presence :true #All notes should have content
+    # validates :content, presence :true
+    # validates :username, presence :true
+    # validates :playlist_id, presence :true #All notes should have content
 
 end
