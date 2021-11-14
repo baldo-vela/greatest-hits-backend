6e91a22035028e8a047aded0f8313d41a35b46e4
baldo-vela <74266847+baldo-vela@users.noreply.github.com>
2021-11-14 12:37:45 -0600

    Rollsback AMS jsonapi standard

--- config/initializers/ams.rb
+++ config/initializers/ams.rb
@@ -1,4 +1,4 @@
 # config/initializers/ams.rb
 #Standard provided by https://jsonapi.org/
 # see https://web-crunch.com/posts/create-a-basic-api-with-ruby-on-rails 
-ActiveModelSerializers.config.adapter = :json_api
+# ActiveModelSerializers.config.adapter = :json_api
